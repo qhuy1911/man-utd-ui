235fe3c036025259504e219bd8f3d03c37cc7bcc
qhuy1911 <qhuy1911@gmail.com>
2022-06-20 04:08:37 +0700

    add header footer

--- src/components/Layouts/Footer/index.js
+++ src/components/Layouts/Footer/index.js
@@ -1,4 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import {
+  faYoutube,
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faTiktok,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames/bind";
+// import { Link } from "react-router-dom";
 import styles from "./Footer.module.scss";
 
 const cx = classNames.bind(styles);
@@ -6,13 +17,54 @@ const cx = classNames.bind(styles);
 function Footer() {
   return (
     <footer className={cx("wrapper")}>
-      {/* Sponsor */}
-      <div className={cx("sponsors")}>
-        <div className={cx("inner1")}></div>
-      </div>
       {/* Social */}
       <div className={cx("socials")}>
-        <div className={cx("inner2")}></div>
+        <ul className={cx("social-list")}>
+          <li className={cx("social-item")}>
+            <a className={cx("social-link")} href="#" target="_blank">
+              <FontAwesomeIcon className={cx("social-icon")} icon={faYoutube} />
+            </a>
+          </li>
+          <li className={cx("social-item")}>
+            <a className={cx("social-link")} href="#" target="_blank">
+              <FontAwesomeIcon
+                className={cx("social-icon")}
+                icon={faFacebookF}
+              />
+            </a>
+          </li>
+          <li className={cx("social-item")}>
+            <a className={cx("social-link")} href="#" target="_blank">
+              <FontAwesomeIcon className={cx("social-icon")} icon={faTwitter} />
+            </a>
+          </li>
+          <li className={cx("social-item")}>
+            <a className={cx("social-link")} href="#" target="_blank">
+              <FontAwesomeIcon
+                className={cx("social-icon")}
+                icon={faInstagram}
+              />
+            </a>
+          </li>
+          <li className={cx("social-item")}>
+            <a className={cx("social-link")} href="#" target="_blank">
+              <FontAwesomeIcon className={cx("social-icon")} icon={faTiktok} />
+            </a>
+          </li>
+        </ul>
+        {/* <div className={cx("social-buttons")}>
+          <Link className={cx("btn-link", "black-btn")} to={"/login"}>
+            Login
+          </Link>
+          <Link className={cx("btn-link", "red-btn")} to={"/signup"}>
+            Sign up
+          </Link>
+        </div> */}
+      </div>
+      {/* Infomation */}
+      <div className={cx("infomations")}>
+        <span>©2022 Chuyên đề Web</span>
+        <span>Khoa CNTT Trường Đại học Nông Lâm TPHCM</span>
       </div>
     </footer>
   );
--- src/components/Layouts/Header/index.js
+++ src/components/Layouts/Header/index.js
@@ -1,4 +1,7 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 const cx = classNames.bind(styles);
@@ -8,7 +11,34 @@ function Header() {
     <div className={cx("wrapper")}>
       <div className={cx("inner")}>
         {/* Menu */}
+        <div className={cx("menu")}>
+          <Link to={"/"}>
+            <img
+              className={cx("menu-logo")}
+              src="https://assets.manutd.com/AssetPicker/images/0/0/3/2/197240/Header-Logo1500994616801.png"
+              alt="ManUtd"
+            />
+          </Link>
+          <ul className={cx("menu-list")}>
+            <li className={cx("menu-item")}>Home</li>
+            <li className={cx("menu-item")}>Shop</li>
+            <li className={cx("menu-item")}>About</li>
+            <li className={cx("menu-item")}>PLayers</li>
+            <li className={cx("menu-item")}>Achievements</li>
+          </ul>
+        </div>
         {/* Search */}
+        <div className={cx("search")}>
+          <input
+            className={cx("search-input")}
+            type="text"
+            placeholder="What are you looking for?"
+          />
+          <FontAwesomeIcon
+            className={cx("search-icon")}
+            icon={faMagnifyingGlass}
+          />
+        </div>
       </div>
     </div>
   );
